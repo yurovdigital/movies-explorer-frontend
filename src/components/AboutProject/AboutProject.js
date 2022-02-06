a0cfca799199bdaddbd2e5f1c27cebd89c2dfe7d
YD <yurovdigital@gmail.com>
2022-02-06 19:01:37 +0300

    fix

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -7,7 +7,7 @@ function AboutProject() {
       <div className="about-project__wrapper">
         <h3 className="about-project__title">О проекте</h3>
         <div className="about-project__content-wrapper">
-          <aticle className="about-project__content">
+          <article className="about-project__content">
             <h4 className="about-project__subtitle">
               Дипломный проект включал 5 этапов
             </h4>
@@ -15,8 +15,8 @@ function AboutProject() {
               Составление плана, работу над бэкендом, вёрстку, добавление
               функциональности и финальные доработки.
             </p>
-          </aticle>
-          <aticle className="about-project__content">
+          </article>
+          <article className="about-project__content">
             <h4 className="about-project__subtitle">
               На выполнение диплома ушло 5 недель
             </h4>
@@ -24,7 +24,7 @@ function AboutProject() {
               У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
               соблюдать, чтобы успешно защититься.
             </p>
-          </aticle>
+          </article>
         </div>
       </div>
       <div className="about-project__bar">
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -6,36 +6,49 @@ import Preloader from '../Preloader/Preloader'
 import MoviesCardList from '../MoviesCardList/MoviesCardList'
 import Footer from '../Footer/Footer'
 
+import { SHORT_MOVIE_DURATION } from '../../utils/constants'
+
 function SavedMovies({
-  movies,
+  // movies,
   loggedIn,
   isLoading,
-  onSubmit,
+  // onSubmit,
   onDelete,
   savedMovies,
   searchKeyword,
 }) {
   const [checkBoxActive, setCheckBoxActive] = React.useState(false)
+  const [filter, setFilter] = React.useState('')
 
   const filterShortMovies = (filterMovies) =>
-    filterMovies.filter((m) => m.duration < 40)
+    filterMovies.filter((m) => m.duration < SHORT_MOVIE_DURATION)
 
   function checkBoxClick() {
     setCheckBoxActive(!checkBoxActive)
   }
 
+  const filteredMovies = React.useMemo(
+    () =>
+      savedMovies.filter((m) =>
+        m.nameRU.toLowerCase().includes(filter.toLowerCase())
+      ),
+    [filter, savedMovies]
+  )
+
   return (
     <>
       <Header loggedIn={loggedIn} />
       <SearchForm
-        onSubmit={onSubmit}
+        onSubmit={setFilter}
         checkBoxClick={checkBoxClick}
         searchKeyword={searchKeyword}
       />
       {isLoading && <Preloader />}
       {!isLoading && (
         <MoviesCardList
-          movies={checkBoxActive ? filterShortMovies(movies) : movies}
+          movies={
+            checkBoxActive ? filterShortMovies(filteredMovies) : filteredMovies
+          }
           onDelete={onDelete}
           savedMovies={savedMovies}
         />
